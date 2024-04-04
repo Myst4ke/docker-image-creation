716f0fb1ecd069e93a6e4d062bccacbfc691d84e
Florian <102389838+Myst4ke@users.noreply.github.com>
2024-04-04 10:59:48 +0200

    Core files

---
+++ tp_image_docker/src/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+
+def main():
+    df = pd.DataFrame([{"name": "Florian", "surname": "Posez", "student number":22320180}])
+    print(df)
+
+if __name__ == "__main__":
+    main()
